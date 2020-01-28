d312f94217048d75ea67971f52297bb6cc5c5240
AlexeiAK <53217691+AlexeiAK@users.noreply.github.com>
2020-01-28 14:44:24 +0200

    Check this file

--- test_items.py
+++ test_items.py
@@ -5,16 +5,21 @@ def test_button_add_to_busket(browser):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     
-    #button = browser.find_element_by_css_selector("#add_to_basket_form > button")
+    button = browser.find_element_by_css_selector("#add_to_basket_form > button")
     #button.klick()
+    time.sleep(10)
     
+def test_button_text(browser):
+
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
     try:
         button = browser.find_element_by_xpath("//button[text()='Add to basket']")
         
         
     except:
         button = browser.find_element_by_css_selector("#add_to_basket_form > button")
-        time.sleep(10)
+        
         if True:
             assert button == True, "Кнопка есть, проверьте название на английском"
         
